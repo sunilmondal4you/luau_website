416a975a4059dc59f9eab5c6adb31d61a9fe68f8
sunilmondal4you <sunilmondal4you@gmail.com>
2019-02-26 19:04:20 +0530

    changes regarding mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Luau Website';
   imgPath = "./assets/img/logo.png";
+  mobileView = false;
+  showNavLink = false;
+
+  ngOnInit() {
+    if (window.screen.width < 768) { // 768px portrait
+      this.mobileView = true;
+    }
+  }
+
+  onResize(event) {
+    if (window.screen.width < 768) { // 768px portrait
+      event.target.innerWidth;
+      this.mobileView = true;
+    }else{
+      event.target.innerWidth;
+      this.mobileView = false;
+    }
+  };
+
+  toggleNavLinks(){
+    this.showNavLink = !this.showNavLink;
+  }
+  
 }
